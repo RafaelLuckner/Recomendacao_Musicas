12e11690b349171af32cccdc3aa1229aeb0989fe
RafaelLuckner <rafaelluckner1@gmail.com>
2025-05-17 18:50:19 -0300

    Novos graficos de nuvem no dash

--- web/pagess/dashboard.py
+++ web/pagess/dashboard.py
@@ -3,6 +3,10 @@ import pandas as pd
 import plotly.express as px
 import sources
 import pytz
+from io import BytesIO
+
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 def clean_session_state():
     valid_keys = ["selected_genres",
@@ -25,6 +29,82 @@ def clean_session_state():
         if key not in valid_keys:
             del st.session_state[key]
 
+def nuvem_palavras(df, coluna):
+            frequencias = df[coluna].value_counts().to_dict()
+            # Criar a nuvem com fundo transparente
+            nuvem = WordCloud(
+                width=800,
+                height=400,
+                background_color=None,
+                mode='RGBA',
+                colormap='viridis',
+                max_words=50
+            ).generate_from_frequencies(frequencias)
+
+            # Salvar a imagem em um buffer de memória
+            buffer = BytesIO()
+            nuvem.to_image().save(buffer, format='PNG')
+            buffer.seek(0)
+
+            # Mostrar no Streamlit
+            st.image(buffer, use_container_width=True)
+
+
+def barchart_top5(df, coluna, BAR_COLOR="#4B7BE5"):
+    df_songs = df[coluna].value_counts().reset_index()
+    df_songs.columns = [coluna, "count"]
+    df_songs = df_songs.sort_values("count", ascending=False).head(5)
+    
+    # Cortar títulos longos
+    df_songs[coluna + "_short"] = df_songs[coluna].apply(lambda x: (x[:27] + "...") if len(x) > 30 else x)
+    
+    # Criar gráfico com cor sólida
+    fig_songs = px.bar(df_songs,
+                        y=coluna + "_short",
+                        x="count",
+                        text="count",
+                        color_discrete_sequence=[BAR_COLOR],
+                        orientation='h')  # Azul petróleo escuro
+    
+    # Ajustes finais
+    fig_songs.update_layout(
+        yaxis={'categoryorder': 'total ascending', 'title': None},
+        xaxis={'visible': False},  # Remove completamente o eixo X
+        margin=dict(l=0, r=0, t=30, b=0), 
+        hoverlabel=dict(bgcolor="white"),
+    )
+    
+    fig_songs.update_traces(
+        textposition='inside',  # Números fora da barra
+        textfont_size=25,
+        hovertemplate="<b>%{y}</b><br>Reproduções: %{x}<extra></extra>"
+    )
+    
+    st.plotly_chart(fig_songs, use_container_width=True)
+
+def tree_map(df):
+    # Contar quantas vezes cada música foi ouvida por gênero
+    df_treemap = df[["genre", "song"]].dropna()
+    df_treemap['genre'] = [genre.capitalize() for genre in df_treemap['genre']]
+    df_treemap = df_treemap.value_counts().reset_index()
+    df_treemap.columns = ["genre", "song", "count"]
+
+    # Criar o treemap hierárquico
+    fig = px.treemap(
+        df_treemap,
+        path=["genre", "song"],
+        values="count",
+    )
+
+    # Deixar o hover limpo e profissional
+    fig.update_traces(
+        hovertemplate='<b>%{label}</b><br>Quantidade: %{value}<extra></extra>'
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
+
+
+
 def show():
     if 'user_id' not in st.session_state or st.session_state["user_id"] == None:
         user_id = sources.search_user_id_mongodb(st.session_state["email"])
@@ -32,203 +112,135 @@ def show():
     if "search_history" not in st.session_state or st.session_state["search_history"] == []:
         st.session_state["search_history"] = sources.search_history_user(st.session_state["user_id"])
 
-    tab1, tab2 = st.tabs(["Dashboard", "Dados da sessão"])
 
-    with tab1:
-        st.session_state["search_history"] = sources.search_history_user(st.session_state["user_id"])
-        # Calcular métricas para todas as músicas ouvidas
-        full_history_df = pd.DataFrame(st.session_state['search_history'])
-        if not full_history_df.empty:
-            if "genre" not in full_history_df.columns:
-                full_history_df["genre"] = None
-            else:
-                full_history_df['genre'] = full_history_df["genre"].fillna('Top 50')
-            total_songs = len(full_history_df)
-            unique_genres = full_history_df["genre"].nunique()
+    st.session_state["search_history"] = sources.search_history_user(st.session_state["user_id"])
+    # Calcular métricas para todas as músicas ouvidas
+    full_history_df = pd.DataFrame(st.session_state['search_history'])
+    if not full_history_df.empty:
+        if "genre" not in full_history_df.columns:
+            full_history_df["genre"] = None
         else:
-            total_songs = 0
-            unique_genres = 0
+            full_history_df['genre'] = full_history_df["genre"].fillna('Top 50')
+        total_songs = len(full_history_df)
+        unique_genres = full_history_df["genre"].nunique()
+    else:
+        total_songs = 0
+        unique_genres = 0
 
-        # Limitar às últimas 100 músicas reproduzidas, ordenando por timestamp
-        recent_history = sorted(st.session_state['search_history'], key=lambda x: x['timestamp'], reverse=True)[:100]
-        df = pd.DataFrame(recent_history)
-        if df.empty:
-            st.write("Nenhuma pesquisa realizada.")
-            return
+    # Limitar às últimas 100 músicas reproduzidas, ordenando por timestamp
+    recent_history = sorted(st.session_state['search_history'], key=lambda x: x['timestamp'], reverse=True)[:100]
+    df = pd.DataFrame(recent_history)
+    if df.empty:
+        st.write("Nenhuma pesquisa realizada.")
+        return
+    else:
+        if "genre" not in df.columns:
+            df["genre"] = None
         else:
-            if "genre" not in df.columns:
-                df["genre"] = None
-            else:
-                df['genre'] = df["genre"].fillna('Top 50')
+            df['genre'] = df["genre"].fillna('Top 50')
 
-            # Convertendo timestamp para data legível
-            df["timestamp"] = pd.to_datetime(df["timestamp"], unit="s")
+        # Convertendo timestamp para data legível
+        df["timestamp"] = pd.to_datetime(df["timestamp"], unit="s")
 
-            # Título do dashboard
-            st.title("🎵 Dashboard Músicas Reproduzidas")
+        # Título do dashboard
+        st.title("🎵 Dashboard Músicas Reproduzidas")
 
-            # Container para estatísticas gerais
-            with st.container():
-                st.subheader("📊 Estatísticas Gerais")
-                col1, col2 = st.columns(2)
-                col1.metric("Total de Músicas", total_songs)
-                col2.metric("Gêneros Únicos", unique_genres)
+        # Container para estatísticas gerais
+        with st.container():
+            st.subheader("📊 Estatísticas Gerais")
+            col1, col2 = st.columns(2)
+            col1.metric("Total de Músicas", total_songs)
+            col2.metric("Gêneros Únicos", unique_genres)
 
-            # Layout de duas colunas para os gráficos
-            col_left, col_right = st.columns(2)
-            with col_left:
-                # Treemap de músicas agrupadas por gênero
-                with st.container(border=True, height=600):
-                    st.subheader("Músicas mais ouvidas por gênero")
+        # Layout de duas colunas para os gráficos
+        col_left, col_right = st.columns(2)
+        with col_left:
 
-                    # Contar quantas vezes cada música foi ouvida por gênero
-                    df_treemap = df[["genre", "song"]].dropna()
-                    df_treemap['genre'] = [genre.capitalize() for genre in df_treemap['genre']]
-                    df_treemap = df_treemap.value_counts().reset_index()
-                    df_treemap.columns = ["genre", "song", "count"]
+            with st.container(border=True, height=470):
+                st.subheader("Músicas Mais Reproduzidas", divider='rainbow')
+                nuvem_palavras(full_history_df, "song")
 
-                    # Criar o treemap hierárquico
-                    fig = px.treemap(
-                        df_treemap,
-                        path=["genre", "song"],
-                        values="count",
-                    )
+            # Gráfico de song mais ouvidos (horizontal)
+            with st.container(border=True, height=600):
+                st.subheader("Top 5 Músicas Mais Ouvidos")
+                BAR_COLOR = "#1a5276"
 
-                    # Deixar o hover limpo e profissional
-                    fig.update_traces(
-                        hovertemplate='<b>%{label}</b><br>Quantidade: %{value}<extra></extra>'
-                    )
+                barchart_top5(full_history_df, "song", BAR_COLOR)
 
-                    st.plotly_chart(fig, use_container_width=True)
+            # Treemap de músicas agrupadas por gênero
+            with st.container(border=True, height=450):
+                st.subheader("Músicas mais ouvidas por gênero")
+                tree_map(full_history_df)
 
-                with st.container(border=True, height=600):
-                    st.subheader("Evolução das Reproduções")
+            # Fuso horário brasileiro
+            fuso_brasil = pytz.timezone("America/Sao_Paulo")
 
-                    # Converter timestamp para datetime com fuso horário brasileiro
-                    fuso_brasil = pytz.timezone("America/Sao_Paulo")
-                    df['datetime'] = pd.to_datetime(df['timestamp'], unit='s').dt.tz_localize('UTC').dt.tz_convert(fuso_brasil)
 
-                    # Opção de agrupamento
-                    periodo = st.radio("Agrupar por:", ["Dia", "Hora"], horizontal=True)
-                    freq = 'D' if periodo == "Dia" else 'h'
-                    date_format = '%d/%m' if freq == 'D' else '%d/%m %Hh'
+        
+        
+        # Coluna da Direita
+        with col_right:
+            # Nu
+            with st.container(border=True, height=470):
+                st.subheader("Artistas Mais Reproduzidos", divider='rainbow')
+                nuvem_palavras(full_history_df, 'artist')
 
-                    # Geração do período completo
-                    time_range = pd.date_range(
-                        start=df['datetime'].min().floor(freq),
-                        end=pd.Timestamp.now(tz=fuso_brasil).ceil(freq),
-                        freq=freq,
-                        tz=fuso_brasil
-                    )
-
-                    df_time = (df.set_index('datetime')
-                              .resample(freq)
-                              .size()
-                              .reindex(time_range, fill_value=0)
-                              .reset_index(name='Reproduções'))
-
-                    df_time.rename(columns={'index': 'Periodo'}, inplace=True)
-                    df_time['Periodo'] = df_time['Periodo'].dt.strftime(date_format)
-
-                    # Gráfico de barras
-                    fig = px.bar(
-                        df_time,
-                        x='Periodo',
-                        y='Reproduções',
-                        labels={'Periodo': 'Período', 'Reproduções': 'Reproduções'}
-                    )
-
-                    fig.update_layout(
-                        height=350,
-                        margin=dict(t=20, b=20, l=10, r=10),
-                        xaxis_title=None,
-                        yaxis_title=None,
-                        showlegend=False,
-                        hovermode='x unified',
-                    )
-
-                    fig.update_traces(marker_color='#1a5276')  # Azul escuro
-
-                    st.plotly_chart(fig, use_container_width=True)
-
-            with col_right:
-                # Gráfico de artistas mais ouvidos (horizontal)
-                with st.container(border=True, height=600):
-                    st.subheader("Top 5 Artistas Mais Ouvidos")
-                    df_artists = df["artist"].dropna().value_counts().reset_index()
-                    df_artists.columns = ["artist", "count"]
-                    df_artists = df_artists.sort_values("count", ascending=False).head(5)
-                    # Cortar nomes com mais de 30 caracteres
-                    df_artists["artist_short"] = df_artists["artist"].apply(lambda x: x[:27] + "..." if len(x) > 30 else x)
-                    
-                    # Cor fixa azul escuro (em formato HEX)
-                    BAR_COLOR = "#1a5276"  # Azul escuro
-                    
-                    fig_artists = px.bar(df_artists, 
-                                       y="artist_short", 
-                                       x="count", 
-                                       orientation='h',
-                                       color_discrete_sequence=[BAR_COLOR],  # Cor uniforme
-                                       text="count")
-                    
-                    fig_artists.update_layout(
-                        yaxis={
-                            'categoryorder': 'total ascending',
-                            'title': None
-                        },
-                        xaxis={
-                            'title': None
-                        },
-                        margin=dict(l=20, r=20, t=40, b=20),
-                        hovermode="y unified",
-                        plot_bgcolor='rgba(0,0,0,0)',  # Fundo transparente
-                        paper_bgcolor='rgba(0,0,0,0)'   # Fundo transparente
-                    )
-                    
-                    fig_artists.update_traces(
-                        textposition='inside',
-                        hovertemplate="<b>%{y}</b><br>Reproduções: %{x}<extra></extra>",
-                        marker=dict(line=dict(width=0)),
-                        textfont_size=25,
-                        # Remove bordas das barras
-                    )
-                    
-                    st.plotly_chart(fig_artists, use_container_width=True)
                 
-                # Gráfico de músicas mais reproduzidas (horizontal)
-                with st.container(border=True, height=600):
-                    st.subheader("Top 5 Músicas Mais Reproduzidas")
-                    df_songs = df["song"].value_counts().reset_index()
-                    df_songs.columns = ["song", "count"]
-                    df_songs = df_songs.sort_values("count", ascending=False).head(5)
-                    
-                    # Cortar títulos longos
-                    df_songs["song_short"] = df_songs["song"].apply(lambda x: (x[:27] + "...") if len(x) > 30 else x)
-                    
-                    # Criar gráfico com cor sólida
-                    fig_songs = px.bar(df_songs,
-                                     y="song_short",
-                                     x="count",
-                                     text="count",
-                                     color_discrete_sequence=[BAR_COLOR],
-                                     orientation='h')  # Azul petróleo escuro
-                    
-                    # Ajustes finais
-                    fig_songs.update_layout(
-                        yaxis={'categoryorder': 'total ascending', 'title': None},
-                        xaxis={'visible': False},  # Remove completamente o eixo X
-                        margin=dict(l=0, r=0, t=30, b=0), 
-                        hoverlabel=dict(bgcolor="white"),
-                    )
-                    
-                    fig_songs.update_traces(
-                        textposition='inside',  # Números fora da barra
-                        textfont_size=25,
-                        hovertemplate="<b>%{y}</b><br>Reproduções: %{x}<extra></extra>"
-                    )
-                    
-                    st.plotly_chart(fig_songs, use_container_width=True)
-            st.write(df)
+            # Gráfico de artistas mais ouvidos (horizontal)
+            with st.container(border=True, height=600):
+                st.subheader("Top 5 Artistas Mais Ouvidos")
+                BAR_COLOR = "#1a5276"
 
-    with tab2:
-        st.write("Dados de sessão:", st.session_state)
+                barchart_top5(full_history_df, "artist", BAR_COLOR)
+            
+
+            with st.container(border=True, height=450):
+                st.subheader("Evolução das Reproduções")
+
+                # Conversão de timestamp
+                full_history_df['datetime'] = pd.to_datetime(full_history_df['timestamp'], unit='s')\
+                    .dt.tz_localize('UTC')\
+                    .dt.tz_convert(fuso_brasil)
+
+                # Agregação diária
+                df_diario = (full_history_df.set_index('datetime')
+                            .resample('D')
+                            .size()
+                            .reset_index(name='Reproduções'))
+
+                df_diario['Data'] = df_diario['datetime'].dt.strftime('%d/%m')
+
+                # Gráfico de área
+                fig = px.area(
+                    df_diario,
+                    x='Data',
+                    y='Reproduções',
+                    labels={'Data': 'Dia', 'Reproduções': 'Reproduções'}
+                )
+
+                fig.update_layout(
+                    height=350,
+                    margin=dict(t=20, b=20, l=10, r=10),
+                    xaxis_title=None,
+                    yaxis_title=None,
+                    showlegend=False,
+                    hovermode='x unified',
+                    plot_bgcolor='rgba(0,0,0,0)',
+                    paper_bgcolor='rgba(0,0,0,0)'
+                )
+
+                fig.update_traces(
+                    line_color='#1a5276',
+                    fill='tozeroy',
+                    hovertemplate='Dia: %{x}<br>Reproduções: %{y}<extra></extra>'
+                )
+
+                st.plotly_chart(fig, use_container_width=True)
+
+
+
+
+        st.write('---')
+        # st.write(df)
+
+  
